b6e43ffd7e66b0297ed160ec0cca4f7bd331be4d
Fraancoch <chianifranco58@gmail.com>
2021-12-10 19:30:06 -0300

    PrimeraSubidaDePrueba

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+let boton = document.getElementById("boton");
+let menu = document.getElementById("menu");
+
+boton.addEventListener("click",function(){
+    "use strict";
+    menu.classList.toggle("mostrar");
+
+});
